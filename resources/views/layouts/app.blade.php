24f3f7d2502041d52d239d9f9c9d7ee429184a0e
Guney Dirim <passarante@gmail.com>
2025-02-25 23:34:06 +0300

    AppLayout Düzenlendi

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,191 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="icon" href="favicon.ico" type="image/x-icon" />
+
+    <link href="https://fonts.googleapis.com/css?family=Nunito+Sans:300,400,600,700,800" rel="stylesheet">
+
+
+    <link rel="stylesheet" href="{{ asset('theme/plugins/bootstrap/dist/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('theme/plugins/fontawesome-free/css/all.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('theme/plugins/icon-kit/dist/css/iconkit.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('theme/plugins/ionicons/dist/css/ionicons.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('theme/plugins/perfect-scrollbar/css/perfect-scrollbar.css') }}">
+    <link rel="stylesheet" href="{{ asset('theme/plugins/datatables.net-bs4/css/dataTables.bootstrap4.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('theme/plugins/jvectormap/jquery-jvectormap.css') }}">
+    <link rel="stylesheet"
+        href="{{ asset('theme/plugins/tempusdominus-bootstrap-4/build/css/tempusdominus-bootstrap-4.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('theme/plugins/weather-icons/css/weather-icons.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('theme/plugins/c3/c3.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('theme/plugins/owl.carousel/dist/assets/owl.carousel.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('theme/plugins/owl.carousel/dist/assets/owl.theme.default.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('theme/dist/css/theme.min.css') }}">
+    <script src="{{ asset('theme/src/js/vendor/modernizr-2.8.3.min.js') }}"></script>
+</head>
+
+<body>
+
+
+    <div class="wrapper">
+        @include('layouts.partials._header')
+
+        <div class="page-wrap">
+            @include('layouts.partials._sidebar')
+
+            {{ $slot }}
+
+
+
+
+
+            <footer class="footer">
+                <div class="clearfix w-100">
+                    <span class="text-center text-sm-left d-md-inline-block">Copyright © 2018 ThemeKit v2.0. All
+                        Rights Reserved.</span>
+                    <span class="float-none mt-1 text-center float-sm-right mt-sm-0">Crafted with <i
+                            class="fa fa-heart text-danger"></i> by <a href="http://lavalite.org/" class="text-dark"
+                            target="_blank">Lavalite</a></span>
+                </div>
+            </footer>
+
+        </div>
+    </div>
+
+
+
+
+    <div class="modal fade apps-modal" id="appsModal" tabindex="-1" role="dialog" aria-labelledby="appsModalLabel"
+        aria-hidden="true" data-backdrop="false">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><i
+                class="ik ik-x-circle"></i></button>
+        <div class="modal-dialog modal-dialog-centered" role="document">
+            <div class="modal-content">
+                <div class="quick-search">
+                    <div class="container">
+                        <div class="row">
+                            <div class="ml-auto mr-auto col-md-4">
+                                <div class="input-wrap">
+                                    <input type="text" id="quick-search" class="form-control"
+                                        placeholder="Search..." />
+                                    <i class="ik ik-search"></i>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="modal-body d-flex align-items-center">
+                    <div class="container">
+                        <div class="apps-wrap">
+                            <div class="app-item">
+                                <a href="#"><i class="ik ik-bar-chart-2"></i><span>Dashboard</span></a>
+                            </div>
+                            <div class="app-item dropdown">
+                                <a href="#" class="dropdown-toggle" role="button" id="dropdownMenuLink"
+                                    data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i
+                                        class="ik ik-command"></i><span>Ui</span></a>
+                                <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+                                    <a class="dropdown-item" href="#">Action</a>
+                                    <a class="dropdown-item" href="#">Another action</a>
+                                    <a class="dropdown-item" href="#">Something else here</a>
+                                </div>
+                            </div>
+                            <div class="app-item">
+                                <a href="#"><i class="ik ik-mail"></i><span>Message</span></a>
+                            </div>
+                            <div class="app-item">
+                                <a href="#"><i class="ik ik-users"></i><span>Accounts</span></a>
+                            </div>
+                            <div class="app-item">
+                                <a href="#"><i class="ik ik-shopping-cart"></i><span>Sales</span></a>
+                            </div>
+                            <div class="app-item">
+                                <a href="#"><i class="ik ik-briefcase"></i><span>Purchase</span></a>
+                            </div>
+                            <div class="app-item">
+                                <a href="#"><i class="ik ik-server"></i><span>Menus</span></a>
+                            </div>
+                            <div class="app-item">
+                                <a href="#"><i class="ik ik-clipboard"></i><span>Pages</span></a>
+                            </div>
+                            <div class="app-item">
+                                <a href="#"><i class="ik ik-message-square"></i><span>Chats</span></a>
+                            </div>
+                            <div class="app-item">
+                                <a href="#"><i class="ik ik-map-pin"></i><span>Contacts</span></a>
+                            </div>
+                            <div class="app-item">
+                                <a href="#"><i class="ik ik-box"></i><span>Blocks</span></a>
+                            </div>
+                            <div class="app-item">
+                                <a href="#"><i class="ik ik-calendar"></i><span>Events</span></a>
+                            </div>
+                            <div class="app-item">
+                                <a href="#"><i class="ik ik-bell"></i><span>Notifications</span></a>
+                            </div>
+                            <div class="app-item">
+                                <a href="#"><i class="ik ik-pie-chart"></i><span>Reports</span></a>
+                            </div>
+                            <div class="app-item">
+                                <a href="#"><i class="ik ik-layers"></i><span>Tasks</span></a>
+                            </div>
+                            <div class="app-item">
+                                <a href="#"><i class="ik ik-edit"></i><span>Blogs</span></a>
+                            </div>
+                            <div class="app-item">
+                                <a href="#"><i class="ik ik-settings"></i><span>Settings</span></a>
+                            </div>
+                            <div class="app-item">
+                                <a href="#"><i class="ik ik-more-horizontal"></i><span>More</span></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+    <script>
+        window.jQuery || document.write('<script src="{{ asset('theme/src/js/vendor/jquery-3.3.1.min.js') }}"><\/script>')
+    </script>
+    <script src="{{ asset('theme/plugins/popper.js/dist/umd/popper.min.js') }}"></script>
+    <script src="{{ asset('theme/plugins/bootstrap/dist/js/bootstrap.min.js') }}"></script>
+    <script src="{{ asset('theme/plugins/perfect-scrollbar/dist/perfect-scrollbar.min.js') }}"></script>
+    <script src="{{ asset('theme/plugins/screenfull/dist/screenfull.js') }}"></script>
+    <script src="{{ asset('theme/plugins/datatables.net/js/jquery.dataTables.min.js') }}"></script>
+    <script src="{{ asset('theme/plugins/datatables.net-bs4/js/dataTables.bootstrap4.min.js') }}"></script>
+    <script src="{{ asset('theme/plugins/datatables.net-responsive/js/dataTables.responsive.min.js') }}"></script>
+    <script src="{{ asset('theme/plugins/datatables.net-responsive-bs4/js/responsive.bootstrap4.min.js') }}"></script>
+    <script src="{{ asset('theme/plugins/jvectormap/jquery-jvectormap.min.js') }}"></script>
+    <script src="{{ asset('theme/plugins/jvectormap/tests/assets/jquery-jvectormap-world-mill-en.js') }}"></script>
+    <script src="{{ asset('theme/plugins/moment/moment.js') }}"></script>
+    <script src="{{ asset('theme/plugins/tempusdominus-bootstrap-4/build/js/tempusdominus-bootstrap-4.min.js') }}">
+    </script>
+    <script src="{{ asset('theme/plugins/d3/dist/d3.min.js') }}"></script>
+    <script src="{{ asset('theme/plugins/c3/c3.min.js') }}"></script>
+    <script src="{{ asset('theme/js/tables.js') }}"></script>
+    <script src="{{ asset('theme/js/widgets.js') }}"></script>
+    <script src="{{ asset('theme/js/charts.js') }}"></script>
+    <script src="{{ asset('theme/dist/js/theme.min.js') }}"></script>
+
+</body>
+
+</html>
+
+
+
+{{-- <!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -21,7 +207,7 @@
             <!-- Page Heading -->
             @isset($header)
                 <header class="bg-white shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                    <div class="px-4 py-6 mx-auto max-w-7xl sm:px-6 lg:px-8">
                         {{ $header }}
                     </div>
                 </header>
@@ -33,4 +219,4 @@
             </main>
         </div>
     </body>
-</html>
+</html> --}}
